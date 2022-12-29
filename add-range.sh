01fa48ee827f0e349b59d98cf973d42e76d48d08
Robert F. Nash <robert@robertnash.net>
2022-12-28 23:15:37 -0500

    fix add-range for times with leading zeros

--- add-range.sh
+++ add-range.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "$1" | sed -e 's/\(..\):\(..\)-\(..\):\(..\)/echo $(date +%F) $(qc 3600*\1+60*\2) $(qc 3600*\3+60*\4)/g'| sh >> periods.txt
+echo "$1" | sed -E -e 's/0([[:digit:]]+)/\1/g' | sed -e 's/\([^:]*\):\([^-]*\)-\([^:]*\):\(.*\)/echo $(date +%F) $(qc 3600*\1+60*\2) $(qc 3600*\3+60*\4)/g' | sh >> periods.txt
 ./data.py -l periods.txt -m 10:30
